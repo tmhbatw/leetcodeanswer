24418be628e53d3673da451b4c0f5ff89e4be95c
tmhbatw <1060226998@qq.com>
2020-12-28 15:45:25 +0800

    更新了Leetcode第14的解答

---
+++ src/leetcode1/Leetcode14.java
@@ -0,0 +1,24 @@
+package leetcode1;
+
+public class Leetcode14 {
+    /*Description
+    * 编写一个函数来查找字符串数组中的最长公共前缀。
+    * 如果不存在公共前缀，返回空字符串 ""。
+    * */
+
+    public String longestCommonPrefix(String[] strs) {
+        if(strs==null||strs.length==0)
+            return "";
+        StringBuilder sb=new StringBuilder();
+        int length=strs.length;
+        for(int i=0;i<strs[0].length();i++){
+            char cur=strs[0].charAt(i);
+            for(int j=1;j<length;j++){
+                if(i==strs[j].length()||cur!=strs[j].charAt(i))
+                    return sb.toString();
+            }
+            sb.append(cur);
+        }
+        return sb.toString();
+    }
+}
