d89bf2d887f60cca641256e5664b49354b361cb8
tmhbatw <1060226998@qq.com>
2020-12-28 18:07:01 +0800

    更新了Leetcode第28的解答

---
+++ src/leetcode1/Leetcode28.java
@@ -0,0 +1,12 @@
+package leetcode1;
+
+public class Leetcode28 {
+    /*Description
+    *给定一个haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。
+    * 如果不存在，则返回  -1。
+    * */
+
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
