dabd2f6b59567df5b559639bb31e14db59ba5db0
tmhbatw <1060226998@qq.com>
2021-01-02 17:24:55 +0800

    更新了Leetcode第81题的答案

---
+++ src/leetcode1/Leetcode81.java
@@ -0,0 +1,16 @@
+package leetcode1;
+
+public class Leetcode81 {
+    /*Description
+    * 假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+    * ( 例如，数组[0,0,1,2,2,5,6]可能变为[2,5,6,0,0,1,2])。
+    * 编写一个函数来判断给定的目标值是否存在于数组中。若存在返回true，否则返回false。
+    * */
+
+    public boolean search(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++)
+            if(nums[i]==target)
+                return true;
+            return false;
+    }
+}
