4bea9bcfc212eae3f03a9e30e283ca22866fb817
tmhbatw <1060226998@qq.com>
2021-01-03 16:53:59 +0800

    更新了Leetcode第96题的答案

---
+++ src/leetcode1/Leetcode96.java
@@ -0,0 +1,28 @@
+package leetcode1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Leetcode96 {
+    /*Description
+    * 给定一个整数 n，求以 1 ... n 为节点组成的二叉搜索树有多少种？
+    * */
+
+    public int numTrees(int n) {
+        Map<Integer,Integer> map=new HashMap<>();
+        return recursion(1,n,map);
+    }
+
+    private int recursion(int start,int end,Map<Integer,Integer> map){
+        if(map.containsKey(end-start))
+            return map.get(end-start);
+        if(start>=end)
+            return 1;
+        int result=0;
+        for(int i=start;i<=end;i++){
+            result+=recursion(start,i-1,map)*recursion(i+1,end,map);
+        }
+        map.put(end-start,result);
+        return result;
+    }
+}
