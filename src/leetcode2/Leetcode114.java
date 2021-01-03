c34c855543d5539ca3832f938841d1ef4f4d7158
tmhbatw <1060226998@qq.com>
2021-01-03 20:12:08 +0800

    更新了Leetcode第114题的答案

---
+++ src/leetcode2/Leetcode114.java
@@ -0,0 +1,24 @@
+package leetcode2;
+
+import datastructure.TreeNode;
+
+public class Leetcode114 {
+    /*Description
+    * 给定一个二叉树，原地将它展开为一个单链表。
+    * */
+
+    public void flatten(TreeNode root) {
+        if(root==null)
+            return ;
+        TreeNode left=root.left;
+        TreeNode right=root.right;
+        TreeNode result=root;
+        result.left=null;
+        flatten(left);
+        root.right=left;
+        while(root.right!=null)
+            root=root.right;
+        flatten(right);
+        root.right=right;
+    }
+}
