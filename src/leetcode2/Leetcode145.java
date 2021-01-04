a6a70989a7ec2d5170482469dfbc8baf2875386b
tmhbatw <1060226998@qq.com>
2021-01-04 20:44:11 +0800

    更新了Leetcode第145题的答案

---
+++ src/leetcode2/Leetcode145.java
@@ -0,0 +1,26 @@
+package leetcode2;
+
+import datastructure.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Leetcode145 {
+    /*Description
+    * 给定一个二叉树，返回它的 后序 遍历。
+     * */
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result=new ArrayList<>();
+        getNumber(root,result);
+        return result;
+    }
+
+    private void getNumber(TreeNode node,List<Integer> result){
+        if(node!=null){
+            getNumber(node.left,result);
+            getNumber(node.right,result);
+            result.add(node.val);
+        }
+    }
+}
