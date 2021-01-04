18ed0802349fe804e3937fb2b8fe9112d274accc
tmhbatw <1060226998@qq.com>
2021-01-04 19:51:38 +0800

    更新了Leetcode第119题的答案

---
+++ src/leetcode2/Leetcode119.java
@@ -0,0 +1,28 @@
+package leetcode2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Leetcode119 {
+    /*Description
+    *给定一个非负索引 k，其中 k ≤ 33，返回杨辉三角的第 k 行。
+    * */
+
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> result=new ArrayList<>();
+        result.add(1);
+        if(rowIndex==0)
+            return result;
+        for(int i=1;i<=rowIndex;i++){
+            List<Integer> cur=new ArrayList<>();
+            cur.add(1);
+            for(int j=0;j<i-1;j++){
+                cur.add(result.get(j)+result.get(j+1));
+            }
+            cur.add(1);
+            result=cur;
+        }
+        return result;
+    }
+
+}
