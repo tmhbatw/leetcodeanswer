1a9011121095564cabdfe609fdd20809ea5debbc
tmhbatw <1060226998@qq.com>
2021-01-04 19:46:25 +0800

    更新了Leetcode第118题的答案

---
+++ src/leetcode2/Leetcode118.java
@@ -0,0 +1,31 @@
+package leetcode2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Leetcode118 {
+    /*Description
+    * 给定一个非负整数 numRows，生成杨辉三角的前 numRows 行
+    * */
+
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result=new ArrayList<>();
+        if(numRows==0)
+            return result;
+        List<Integer> res=new ArrayList<>();
+        res.add(1);
+        result.add(res);
+        for(int i=1;i<numRows;i++){
+            List<Integer> cur=new ArrayList<>();
+            cur.add(1);
+            for(int j=0;j<i-1;j++){
+                cur.add(res.get(j)+res.get(j+1));
+            }
+            cur.add(1);
+            result.add(cur);
+            res=cur;
+        }
+        return result;
+    }
+}
