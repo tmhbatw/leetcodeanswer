b65f7d613a1dfd7f0f92ca4c96544765c311c570
tmhbatw <1060226998@qq.com>
2021-01-06 00:08:17 +0800

    更新了第168题的答案

---
+++ src/leetcode2/Leetcode168.java
@@ -0,0 +1,18 @@
+package leetcode2;
+
+public class Leetcode168 {
+    /*Description
+    * 给定一个正整数，返回它在 Excel 表中相对应的列名称。
+    * */
+
+    public String convertToTitle(int n) {
+        StringBuilder sb=new StringBuilder();
+        while(n!=0){
+            n--;
+            int cur=n%26;
+            sb.append((char)(cur+'A'));
+            n/=26;
+        }
+        return sb.reverse().toString();
+    }
+}
