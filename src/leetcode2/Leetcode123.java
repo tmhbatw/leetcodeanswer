5c08a34b3c57c949ed800cdaf63e4d7225bcda0c
tmhbatw <1060226998@qq.com>
2021-01-05 13:25:24 +0800

    更新了Leetcode第123题的答案

--- src/leetcode2/Leetcode123.java
+++ src/leetcode2/Leetcode123.java
@@ -9,6 +9,40 @@ public class Leetcode123 {
     * 注意:你不能同时参与多笔交易（你必须在再次购    买前出售掉之前的股票）。
     * */
 
+    //dp[5][length]分别保存没有操作，买了第一支股票、卖了第一支股票、
+    // 买了第二支股票、卖了第二支股票五种状态
+
+    public int maxProfit(int[] prices) {
+        if(prices==null||prices.length<=1)
+            return 0;
+        int pLength=prices.length;
+        if(pLength==2){
+            return Math.max(0,prices[1]-prices[0]);
+        }
+        if(pLength==3){
+            return Math.max(Math.max(0,prices[1]-prices[0]),prices[2]-Math.min(prices[0],prices[1]));
+        }
+        int[][] dp=new int[5][pLength];
+        //初始化这个dp数组
+        dp[1][0]=-prices[0];
+        for(int j=1;j<4;j++)
+            dp[1][j]=Math.max(dp[1][0],-prices[j]);
+        dp[2][1]=-prices[0]+prices[1];
+        for(int j=2;j<4;j++)
+            dp[2][j]=Math.max(dp[2][j-1],dp[1][j-1]+prices[j]);
+        dp[3][2]=dp[2][1]-prices[2];
+        dp[3][3]=Math.max(dp[3][2],dp[2][2]-prices[3]);
+        dp[4][3]=dp[3][2]+prices[3];
+        for(int i=4;i<pLength;i++){
+            dp[1][i]=Math.max(dp[1][i-1],dp[0][i-1]-prices[i]);
+            dp[2][i]=Math.max(dp[2][i-1],dp[1][i-1]+prices[i]);
+            dp[3][i]=Math.max(dp[3][i-1],dp[2][i-1]-prices[i]);
+            dp[4][i]=Math.max(dp[4][i-1],dp[3][i-1]+prices[i]);
+        }
+        for(int[] cur:dp)
+            System.out.println(Arrays.toString(cur));
+        return Math.max(0,Math.max(dp[2][pLength-1],dp[4][pLength-1]));
+    }
     //保存三个数组，分别用来存储当前持有一支股票、两支股票以及不持有股票的最大值
 /*    public int maxProfit(int[] prices) {
         if(prices==null||prices.length<=1)
