24dcb471cbd6cff90d07b493265fb8ec66401f37
tmhbatw <1060226998@qq.com>
2021-01-05 23:31:06 +0800

    更新了第172题的答案

---
+++ src/leetcode2/Leetcode172.java
@@ -0,0 +1,16 @@
+package leetcode2;
+
+public class Leetcode172 {
+    /*Description
+    * 给定一个整数 n，返回 n! 结果尾数中零的数量。
+    * */
+    public int trailingZeroes(int n) {
+        int result=0;
+        while(n!=0){
+            n/=5;
+            result+=n;
+        }
+        return result;
+    }
+
+}
