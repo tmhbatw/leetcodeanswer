af1e634cd45e167cc339d0c93b3133830a5cfcaa
tmhbatw <1060226998@qq.com>
2021-01-06 00:57:36 +0800

    更新了第157题的答案

---
+++ src/leetcode2/Leetcode157.java
@@ -0,0 +1,11 @@
+package leetcode2;
+
+public class Leetcode157 {
+    /*Description
+    * 给你一个文件，并且该文件只能通过给定的 read4 方法来读取，请实现一个方法使其能够读取 n 个字符。
+    * */
+
+    public int read(char[] buf, int n) {
+
+    }
+}
