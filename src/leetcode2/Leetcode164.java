e20ea980be99a93f70c75a91a9c7db8e42fb3854
tmhbatw <1060226998@qq.com>
2021-01-06 01:41:10 +0800

    更新了Leetcode第164题的答案

---
+++ src/leetcode2/Leetcode164.java
@@ -0,0 +1,20 @@
+package leetcode2;
+
+import java.util.Arrays;
+
+public class Leetcode164 {
+    /*Description
+    * 给定一个无序的数组，找出数组在排序之后，相邻元素之间最大的差值。
+    * 如果数组元素个数小于 2，则返回 0。
+    * */
+
+    public int maximumGap(int[] nums) {
+        if(nums==null||nums.length==0)
+            return 0;
+        Arrays.sort(nums);
+        int res=0;
+        for(int i=1;i<nums.length;i++)
+            res=Math.max(nums[i]-nums[i-1],res);
+        return res;
+    }
+}
