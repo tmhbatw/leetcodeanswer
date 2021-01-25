18a5a473e70062fe7e0c8edfee3c3b283cf3a708
tmhbatw <1060226998@qq.com>
2021-01-26 02:24:18 +0800

    更新了Leetcode第346题的答案

---
+++ src/leetcode4/Leetcode346.java
@@ -0,0 +1,30 @@
+package leetcode4;
+
+public class Leetcode346 {
+    /*Description
+    * 给定一个整数数据流和一个窗口大小，根据该滑动窗口的大小，计算其所有整数的移动平均值。
+    * */
+
+    class MovingAverage {
+        int number=0;
+        int index=0;
+        int sum=0;
+        int capacity;
+        int[] num;
+
+        /** Initialize your data structure here. */
+        public MovingAverage(int size) {
+            num=new int[size];
+            capacity=size;
+        }
+
+        public double next(int val) {
+            sum-=num[index]-val;
+            num[index++]=val;
+            if(index==capacity)
+                index=0;
+            number++;
+            return 1.0*sum/Math.min(number,capacity);
+        }
+    }
+}
