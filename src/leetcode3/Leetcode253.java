0c715a0801fc1e45126f4f8445d3f7727247996d
tmhbatw <1060226998@qq.com>
2021-02-06 01:03:58 +0800

    更新了Leetcode253题的新解法

--- src/leetcode3/Leetcode253.java
+++ src/leetcode3/Leetcode253.java
@@ -1,5 +1,6 @@
 package leetcode3;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
@@ -11,6 +12,36 @@ public class Leetcode253 {
     * */
 
     public int minMeetingRooms(int[][] intervals) {
+        if(intervals==null||intervals.length==0)
+            return 0;
+        int length= intervals.length;
+        int[] start=new int[length];
+        int[] end=new int[length];
+        for(int i=0;i<length;i++){
+            start[i]=intervals[i][0];
+            end[i]=intervals[i][1];
+        }
+        Arrays.sort(start);
+        Arrays.sort(end);
+        System.out.println(Arrays.toString(start));
+        System.out.println(Arrays.toString(end));
+        int sIndex=0,eIndex=0;
+        int result=0;
+        int max=0;
+        while(sIndex<length&&eIndex<length){
+            if(end[eIndex]<=start[sIndex]){
+                eIndex++;
+                result--;
+            }else{
+                result++;
+                max=Math.max(result,max);
+                sIndex++;
+            }
+        }
+        return max;
+    }
+    //TreeMap算法，时间复杂度很垃圾
+/*    public int minMeetingRooms(int[][] intervals) {
         TreeMap<Integer,Integer> map=new TreeMap<>();
         for(int[] cur:intervals){
             map.put(cur[0],map.getOrDefault(cur[0],0)+1);
@@ -24,5 +55,5 @@ public class Leetcode253 {
             max=Math.max(max,result);
         }
         return max;
-    }
+    }*/
 }
