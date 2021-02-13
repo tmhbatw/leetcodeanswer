c05de229f2eb5de0c2658392c6f7900555a8512a
tmhbatw <1060226998@qq.com>
2021-02-14 03:02:20 +0800

    更新了Leetcode553题的新解法

---
+++ src/leetcode6/Leetcode553.java
@@ -0,0 +1,25 @@
+package leetcode6;
+
+public class Leetcode553 {
+    /*Description
+    * 给定一组正整数，相邻的整数之间将会进行浮点除法操作。例如，[2,3,4] -> 2 / 3 / 4 。
+    * 但是，你可以在任意位置添加任意数目的括号，来改变算数的优先级。你需要找出怎么添加括号，
+    * 才能得到最大的结果，并且返回相应的字符串格式的表达式。你的表达式不应该含有冗余的括号。
+    * */
+
+    public String optimalDivision(int[] nums) {
+        if(nums.length==1)
+            return nums[0]+"";
+        if(nums.length==2)
+            return nums[0]+"/"+nums[1];
+        StringBuilder result=new StringBuilder();
+        result.append(nums[0]);
+        result.append("/").append("(");
+        result.append(nums[1]);
+        for(int i=2;i<nums.length;i++){
+            result.append("/").append(nums[i]);
+        }
+        result.append(")");
+        return result.toString();
+    }
+}
