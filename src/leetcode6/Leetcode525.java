91a3595f8495e012fdb8fb4924dc449259c61ab2
tmhbatw <1060226998@qq.com>
2021-02-14 04:19:52 +0800

    更新了Leetcode525题的新解法

---
+++ src/leetcode6/Leetcode525.java
@@ -0,0 +1,28 @@
+package leetcode6;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Leetcode525 {
+    /*Description
+    * 给定一个二进制数组, 找到含有相同数量的 0 和 1 的最长连续子数组（的长度）。
+    * */
+
+    public int findMaxLength(int[] nums) {
+        Map<Integer,Integer> map=new HashMap<>();
+        map.put(0,-1);
+        int result=0;
+        int preSum=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0)
+                preSum++;
+            else
+                preSum--;
+            if(map.containsKey(preSum))
+                result=Math.max(result,i-map.get(preSum));
+            else
+                map.put(preSum,i);
+        }
+        return result;
+    }
+}
