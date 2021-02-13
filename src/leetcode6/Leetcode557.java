b8c3acdab249ba9e45cf49a447bd5ebd60865247
tmhbatw <1060226998@qq.com>
2021-02-13 17:37:18 +0800

    更新了Leetcode557题的新解法

---
+++ src/leetcode6/Leetcode557.java
@@ -0,0 +1,15 @@
+package leetcode6;
+
+public class Leetcode557 {
+    /*Description
+    * 给定一个字符串，你需要反转字符串中每个单词的字符顺序，同时仍保留空格和单词的初始顺序。
+    * */
+
+    public String reverseWords(String s) {
+        String[] ss=s.split(" ");
+        StringBuilder sb=new StringBuilder();
+        for(String cur:ss)
+            sb.append(" ").append(new StringBuilder(cur).reverse());
+        return sb.substring(1);
+    }
+}
