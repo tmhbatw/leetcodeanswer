ecb46b853a7335997f3f8674b7166767772536b4
tmhbatw <1060226998@qq.com>
2021-02-14 12:09:56 +0800

    更新了Leetcode589题的新解法

---
+++ src/leetcode6/Leetcode589.java
@@ -0,0 +1,42 @@
+package leetcode6;
+
+import datastructure.Node;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Leetcode589 {
+    /*Description
+    * 给定一个 N 叉树，返回其节点值的前序遍历。
+     * */
+
+    class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    };
+
+    public List<Integer> preorder(Node root) {
+        List<Integer> result=new ArrayList<>();
+        preOrder(root,result);
+        return result;
+    }
+
+    private void preOrder(Node root,List<Integer> list){
+        if(root!=null){
+            list.add(root.val);
+            for(Node cur:root.children)
+                preOrder(cur,list);
+        }
+    }
+}
