85c1feecbd43fd4766d7c6ceb26c531bc90c23d3
tmhbatw <1060226998@qq.com>
2021-02-15 01:52:51 +0800

    更新了Leetcode617题的新解法

---
+++ src/leetcode7/Leetcode617.java
@@ -0,0 +1,22 @@
+package leetcode7;
+
+import datastructure.TreeNode;
+
+public class Leetcode617 {
+    /*Description
+    * 给定两个二叉树，想象当你将它们中的一个覆盖到另一个上时，两个二叉树的一些节点便会重叠。
+    * 你需要将他们合并为一个新的二叉树。合并的规则是如果两个节点重叠，
+    * 那么将他们的值相加作为节点合并后的新值，否则不为NULL 的节点将直接作为新二叉树的节点。
+    * */
+
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if(root1==null)
+            return root2;
+        if(root2==null)
+            return root1;
+        TreeNode result=new TreeNode(root1.val+root2.val);
+        result.left=mergeTrees(root1.left,root2.left);
+        result.right=mergeTrees(root1.right,root2.right);
+        return result;
+    }
+}
