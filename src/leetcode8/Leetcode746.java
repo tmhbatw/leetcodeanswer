478caef8911aa46078a60c2eb54671279c3b0252
tmhbatw <1060226998@qq.com>
2021-03-01 00:17:59 +0800

    更新了Leetcode746题的新解法

---
+++ src/leetcode8/Leetcode746.java
@@ -0,0 +1,18 @@
+package leetcode8;
+
+public class Leetcode746 {
+    /*Description
+    * 数组的每个下标作为一个阶梯，第 i 个阶梯对应着一个非负数的体力花费值 cost[i]（下标从 0 开始）。
+    * 每当你爬上一个阶梯你都要花费对应的体力值，一旦支付了相应的体力值，你就可以选择向上爬一个阶梯或者爬两个阶梯。
+    * 请你找出达到楼层顶部的最低花费。在开始时，你可以选择从下标为 0 或 1 的元素作为初始阶梯。
+    * */
+
+    public int minCostClimbingStairs(int[] cost) {
+        int length=cost.length;
+        int[] dp=new int[length+1];
+        for(int i=2;i<=length;i++){
+            dp[i]=Math.min(dp[i-1]+cost[i-1],dp[i-2]+cost[i-2]);
+        }
+        return dp[length];
+    }
+}
