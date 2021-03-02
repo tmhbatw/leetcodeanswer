d90152a4204cbf9a515adaf3c30e06eef8534fbf
tmhbatw <1060226998@qq.com>
2021-03-03 03:38:28 +0800

    更新了剑指offer第53_2题的答案

---
+++ src/forOffer/forOffer53_2.java
@@ -0,0 +1,22 @@
+package forOffer;
+
+public class forOffer53_2 {
+    /*Description
+    * 一个长度为n-1的递增排序数组中的所有数字都是唯一的，并且每个数字都在范围0～n-1之内。
+    * 在范围0～n-1内的n个数字中有且只有一个数字不在该数组中，请找出这个数字。
+    * */
+
+    public int missingNumber(int[] nums) {
+        int left=0,right=nums.length-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]==mid)
+                left=mid+1;
+            else if(nums[mid]!=mid&&(mid==0||nums[mid-1]==mid-1))
+                return mid;
+            else
+                right=mid-1;
+        }
+        return -1;
+    }
+}
