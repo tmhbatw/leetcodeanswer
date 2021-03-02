90b8a491f1061a393efd6b05e5a26e12632353e1
tmhbatw <1060226998@qq.com>
2021-03-02 16:04:41 +0800

    更新了剑指offer第14_1题的答案

---
+++ src/forOffer/forOffer14_1.java
@@ -0,0 +1,20 @@
+package forOffer;
+
+public class forOffer14_1 {
+    /*Description
+    * 给你一根长度为 n 的绳子，请把绳子剪成整数长度的 m 段（m、n都是整数，n>1并且m>1），
+    * 每段绳子的长度记为 k[0],k[1]...k[m-1] 。请问 k[0]*k[1]*...*k[m-1] 可能的最大乘积是多少？
+    * 例如，当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此时得到的最大乘积是18。
+    * */
+
+    public int cuttingRope(int n) {
+        if(n<=3)
+            return n-1;
+        if(n%3==0)
+            return (int) Math.pow(3,n/3);
+        if(n%3==1)
+            return (int) Math.pow(3,n/3-1)*4;
+        return 2*(int)Math.pow(3,n/3);
+
+    }
+}
