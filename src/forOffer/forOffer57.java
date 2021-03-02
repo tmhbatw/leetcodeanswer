9e6563d8838f0e102e154ab935a00fa8a2ce2074
tmhbatw <1060226998@qq.com>
2021-03-03 04:16:51 +0800

    更新了剑指offer第57题的答案

---
+++ src/forOffer/forOffer57.java
@@ -0,0 +1,20 @@
+package forOffer;
+
+public class forOffer57 {
+    /*Description
+    * 输入一个递增排序的数组和一个数字s，在数组中查找两个数，使得它们的和正好是s。如果有多对数字的和等于s，则输出任意一对即可。
+    * */
+
+    public int[] twoSum(int[] nums, int target) {
+        int left=0,right=nums.length-1;
+        while(left<right){
+            if(nums[left]+nums[right]==target)
+                return new int[]{nums[left],nums[right]};
+            else if(nums[left]+nums[right]>target)
+                right--;
+            else
+                left++;
+        }
+        return null;
+    }
+}
