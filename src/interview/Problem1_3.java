3b8666e7c24768c7b79256b172ab4bfde17fafbe
tmhbatw <1060226998@qq.com>
2021-03-03 21:11:32 +0800

    更新了面经第1_3题

--- src/interview/Problem1_3.java
+++ src/interview/Problem1_3.java
@@ -8,7 +8,7 @@ public class Problem1_3 {
 
     public String replaceSpaces(String S, int length) {
         StringBuilder sb=new StringBuilder();
-        for(int i=0;i<S.length();i++){
+        for(int i=0;i<Math.min(S.length(),length);i++){
             if(S.charAt(i)==' ')
                 sb.append("%20");
             else
