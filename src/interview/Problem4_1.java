ac6a16b05218d012364d860300fea9a0d4c5afc7
tmhbatw <1060226998@qq.com>
2021-03-04 15:13:47 +0800

    更新了面经4_1题目解答

---
+++ src/interview/Problem4_1.java
@@ -0,0 +1,37 @@
+package interview;
+
+import java.util.*;
+
+public class Problem4_1 {
+    /*Description
+    * 节点间通路。给定有向图，设计一个算法，找出两个节点之间是否存在一条路径。
+    * */
+
+    public boolean findWhetherExistsPath(int n, int[][] graph, int start, int target) {
+        List<Set<Integer>> list=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            list.add(new HashSet<>());
+        }
+        for(int[] cur:graph) {
+           Set<Integer> set=list.get(cur[0]);
+           set.add(cur[1]);
+        }
+        boolean[] reached=new boolean[n];
+        reached[start]=true;
+        Queue<Integer> q=new LinkedList<>();
+        q.add(start);
+        while(!q.isEmpty()){
+            int cur=q.poll();
+            Set<Integer> set=list.get(cur);
+            for(int i:set){
+                if(!reached[i]){
+                    if(i==target)
+                        return true;
+                    reached[i]=true;
+                    q.add(i);
+                }
+            }
+        }
+        return false;
+    }
+}
