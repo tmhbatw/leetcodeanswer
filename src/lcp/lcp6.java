f57fbd30b2427564dab353f63efea0a139e1542e
tmhbatw <1060226998@qq.com>
2021-04-12 17:48:21 +0800

    更新了lcp第6题

---
+++ src/lcp/lcp6.java
@@ -0,0 +1,16 @@
+package lcp;
+
+public class lcp6 {
+    /*Description
+    * 桌上有 n 堆力扣币，每堆的数量保存在数组 coins 中。我们每次可以选择任意一堆，
+    * 拿走其中的一枚或者两枚，求拿完所有力扣币的最少次数。
+    * */
+
+    public int minCount(int[] coins) {
+        int result=0;
+        for(int cur:coins){
+            result+=(cur+1)/2;
+        }
+        return result;
+    }
+}
