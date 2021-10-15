e6c3e1327361ce3c4e889dc19d4cf8717fc4bcdf
tmhbatw <1060226998@qq.com>
2021-10-15 16:35:32 +0800

    更新了leetcode1365题的答案

---
+++ src/leetcode/leetcode14/Leetcode1365.java
@@ -0,0 +1,15 @@
+package leetcode.leetcode14;
+
+public class Leetcode1365 {
+
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] res=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;j<nums.length;j++){
+                if(nums[i]>nums[j])
+                    res[i]++;
+            }
+        }
+        return res;
+    }
+}
