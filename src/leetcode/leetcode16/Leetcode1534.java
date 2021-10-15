938d3fe7820a2f83659aee13935e84d783e1d136
tmhbatw <1060226998@qq.com>
2021-10-15 23:51:54 +0800

    更新了leetcode1534题的答案

---
+++ src/leetcode/leetcode16/Leetcode1534.java
@@ -0,0 +1,19 @@
+package leetcode.leetcode16;
+
+public class Leetcode1534 {
+
+    public int countGoodTriplets(int[] arr, int a, int b, int c) {
+        int count=0;
+        for(int i=0;i<arr.length-2;i++){
+            for(int j=i+1;j<arr.length-1;j++){
+                for(int k=j+1;k<arr.length;k++){
+                    if(Math.abs(arr[j]-arr[i])<=a&&
+                        Math.abs(arr[k]-arr[j])<=b&&
+                        Math.abs(arr[i]-arr[k])<=c)
+                        count++;
+                }
+            }
+        }
+        return count;
+    }
+}
