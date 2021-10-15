a389a39c4b728f4c0b9507c858cdaf454cc08d51
tmhbatw <1060226998@qq.com>
2021-10-15 21:38:50 +0800

    更新了leetcode1450题的答案

---
+++ src/leetcode/leetcode15/Leetcode1450.java
@@ -0,0 +1,13 @@
+package leetcode.leetcode15;
+
+public class Leetcode1450 {
+
+    public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+        int result=0;
+        for(int i=0;i<startTime.length;i++){
+            if(startTime[i]<=queryTime&&endTime[i]>=queryTime)
+                result++;
+        }
+        return result;
+    }
+}
