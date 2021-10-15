a158f6b61e5de7ed3aea8713f87758840b43a619
tmhbatw <1060226998@qq.com>
2021-10-15 23:29:48 +0800

    更新了leetcode1528题的答案

---
+++ src/leetcode/leetcode16/Leetcode1528.java
@@ -0,0 +1,11 @@
+package leetcode.leetcode16;
+
+public class Leetcode1528 {
+
+    public String restoreString(String s, int[] indices) {
+        char[] res=new char[s.length()];
+        for(int i=0;i<indices.length;i++)
+            res[indices[i]]=s.charAt(i);
+        return new String(res);
+    }
+}
