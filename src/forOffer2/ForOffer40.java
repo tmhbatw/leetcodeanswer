73a77cbc2d2be91051c5d6504109498656d6169b
tmhbatw <1060226998@qq.com>
2021-10-20 01:42:07 +0800

    更新了forOffer40题的答案

--- src/forOffer2/ForOffer40.java
+++ src/forOffer2/ForOffer40.java
@@ -3,6 +3,7 @@ package forOffer2;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Stack;
 
 public class ForOffer40 {
 
@@ -35,6 +36,43 @@ public class ForOffer40 {
             result=Math.max(result,sum1[index]+sum2[index2]);
         }
 
+        return result;
+    }
+
+
+    public int maximalRectangle(String[] matrix) {
+        char[][] m=new char[matrix.length][matrix[0].length()];
+        for(int i=0;i<matrix.length;i++)
+            m[i]=matrix[i].toCharArray();
+        if(m==null||m.length==0)
+            return 0;
+        int row=m.length,col=m[0].length;
+        int[][] time=new int[row+1][col];
+        int result=0;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(m[i][j]=='1')
+                    time[i+1][j]=time[i][j]+1;
+            }
+            int[] left=new int[col];
+            Stack<int[]> stack=new Stack<>();
+            stack.add(new int[]{-1,-1});
+            for(int j=0;j<col;j++){
+                while(stack.peek()[0]>=time[i+1][j])
+                    stack.pop();
+                left[j]=stack.peek()[1];
+                stack.add(new int[]{time[i+1][j],j});
+            }
+            stack=new Stack<>();
+            stack.add(new int[]{-1,col});
+            for(int j=col-1;j>=0;j--){
+                while(stack.peek()[0]>=time[i+1][j])
+                    stack.pop();
+                int right=stack.peek()[1];
+                result=Math.max(result,(right-left[j]-1)*time[i+1][j]);
+                stack.add(new int[]{time[i+1][j],j});
+            }
+        }
         return result;
     }
 }
