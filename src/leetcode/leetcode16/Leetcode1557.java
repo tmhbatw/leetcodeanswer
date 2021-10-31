18efdc3c0c9aad57ae6800e73d862ee6cfba955c
tmhbatw <1060226998@qq.com>
2021-10-31 19:37:33 +0800

    更新了Leetcode1557题的答案

--- src/leetcode/leetcode16/Leetcode1557.java
+++ src/leetcode/leetcode16/Leetcode1557.java
@@ -1,11 +1,20 @@
 package leetcode.leetcode16;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Leetcode1557 {
 
     public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
-        return null;
-
+        boolean[] degree=new boolean[n];
+        for(List<Integer> edge:edges){
+            degree[edge.get(1)]=true;
+        }
+        List<Integer> result=new ArrayList<>();
+        for(int i=0;i<degree.length;i++){
+            if(!degree[i])
+                result.add(i);
+        }
+        return result;
     }
 }
