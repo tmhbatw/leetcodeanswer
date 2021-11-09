ad700c559b86fc68f1f1b4f1e839c9545a04d7e7
tmhbatw <1060226998@qq.com>
2021-11-09 20:34:11 +0800

    更新了Leetcode912题的答案

---
+++ src/leetcode/leetcode10/Leetcode912.java
@@ -0,0 +1,11 @@
+package leetcode.leetcode10;
+
+import java.util.Arrays;
+
+public class Leetcode912 {
+
+    public int[] sortArray(int[] nums) {
+        Arrays.sort(nums);
+        return nums;
+    }
+}
