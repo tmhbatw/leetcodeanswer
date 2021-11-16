9208e6f0f360458f62d5a02b9cc1e09745b4bd1b
tmhbatw <1060226998@qq.com>
2021-11-16 17:43:07 +0800

    更新了Leetcode424题的答案

--- src/leetcode/leetcode5/Leetcode424.java
+++ src/leetcode/leetcode5/Leetcode424.java
@@ -3,25 +3,30 @@ package leetcode.leetcode5;
 public class Leetcode424 {
 
     public int characterReplacement(String s, int k) {
+        int length=s.length();
         int result=0;
-        for(int i=0;i<26;i++){
-            result=Math.max(result,characterReplacement(s,k,(char)(i+'A')));
+        int j=0;
+        int[] num=new int[26];
+
+        for(int i=0;i<length;i++){
+            while(j<length&&isMatch(num,s.charAt(j),k)){
+                num[s.charAt(j++)-'A']++;
+            }
+            result=Math.max(j-i,result);
+            num[s.charAt(i)-'A']--;
         }
         return result;
     }
 
-    private int characterReplacement(String s, int k, char cur){
-        int length=s.length();
-        int result=0;
-        int j=0;
-        for(int i=0;i<length;i++){
-            while(j<length&&k-(s.charAt(j)==cur?0:1)>=0) {
-                k-=(s.charAt(j++)==cur?0:1);
-            }
-            result=Math.max(result,j-i);
-            if(s.charAt(i)!=cur)
-                k++;
+    private boolean isMatch(int[] num,char cur,int k){
+        num[cur-'A']++;
+        int max=0;
+        int sum=0;
+        for(int n:num){
+            max=Math.max(n,max);
+            sum+=n;
         }
-        return result;
+        num[cur-'A']--;
+        return sum-max<=k;
     }
 }
