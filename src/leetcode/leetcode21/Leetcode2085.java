a64ce0c358509447318c0ff296b728ec4a2c2ca6
liu <1060226998@qq.com>
2021-12-01 00:56:22 +0800

    更新了leetcode第2085题的答案

--- src/leetcode/leetcode21/Leetcode2085.java
+++ src/leetcode/leetcode21/Leetcode2085.java
@@ -1,6 +1,8 @@
 package leetcode.leetcode21;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Leetcode2085 {
 
@@ -11,5 +13,25 @@ public class Leetcode2085 {
         for(int i=0;i<seats.length;i++)
             res+=Math.abs(seats[i]-students[i]);
         return res;
+    }
+
+    public int countWords(String[] words1, String[] words2) {
+        Map<String,Integer> map1=getMap(words1);
+        Map<String,Integer> map2=getMap(words2);
+
+        int result=0;
+        for(String key:map1.keySet()){
+            if(map1.get(key)==1&&map2.getOrDefault(key,0)==1)
+                result++;
+        }
+        return result;
+    }
+
+    private Map<String,Integer> getMap(String[] word){
+        Map<String,Integer> map=new HashMap<>();
+        for(String cur:word){
+            map.put(cur,map.getOrDefault(cur,0)+1);
+        }
+        return map;
     }
 }
