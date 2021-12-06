271232150dc6923744cb364522f28227d3a930d7
tmhbatw <1060226998@qq.com>
2021-12-06 21:15:27 +0800

    更新了leetcode 1105题的答案

--- src/leetcode/leetcode12/Leetcode1105.java
+++ src/leetcode/leetcode12/Leetcode1105.java
@@ -2,19 +2,24 @@ package leetcode.leetcode12;
 
 public class Leetcode1105 {
     public int minHeightShelves(int[][] books, int shelfWidth) {
-        int result=0;
         int length=books.length;
+        int[] dp=new int[length+1];
 
         for(int i=0;i<length;i++){
 
-            int cur=books[i][0];
-            int max=books[i][1];
-            while(i<length-1&&cur+books[i+1][0]<=shelfWidth){
-                cur+=books[++i][0];
-                max=Math.max(max,books[i][1]);
+            dp[i+1]=dp[i]+books[i][1];
+            int curWidth=books[i][0];
+            int maxHeight=books[i][1];
+
+            for(int j=i-1;j>=0;j--){
+                curWidth+=books[j][0];
+                if(curWidth>shelfWidth)
+                    break;
+                maxHeight=Math.max(maxHeight,books[j][1]);
+                dp[i+1]=Math.min(dp[i+1],dp[j]+maxHeight);
             }
-            result+=max;
         }
-        return result;
+
+        return dp[length];
     }
 }
