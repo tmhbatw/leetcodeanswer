ab6f896de0cd99812f135c891c2921093ec0cbea
tmhbatw <1060226998@qq.com>
2021-12-07 21:35:04 +0800

    更新了Leetcode第388题的答案

---
+++ src/leetcode/leetcode4/Leetcode388.java
@@ -0,0 +1,33 @@
+package leetcode.leetcode4;
+
+import sun.misc.ASCIICaseInsensitiveComparator;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class Leetcode388 {
+
+    public int lengthLongestPath(String input) {
+        String[] s=input.split("\n");
+        Stack<Integer> s1=new Stack<>();
+        Stack<Integer> s2=new Stack<>();
+        int result=0;
+
+        for(String ss:s){
+            int index=0;
+            while(ss.charAt(index)=='\t')
+                index++;
+            String curS=ss.substring(index);
+            while(!s2.isEmpty()&&s2.peek()>=index){
+                s2.pop();
+                s1.pop();
+            }
+            int curLength=(s1.isEmpty()?0:s1.peek())+curS.length()+1;
+
+            result=Math.max(result,curLength);
+            s1.add(curLength);
+            s2.add(index);
+        }
+        return result-1;
+    }
+}
