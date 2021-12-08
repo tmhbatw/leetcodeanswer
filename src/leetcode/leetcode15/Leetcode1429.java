5c01c3dbd83a534d6c7891ba02852f9269362372
tmhbatw <1060226998@qq.com>
2021-12-08 22:01:30 +0800

    更新了Leetcode 第1429题的答案

--- src/leetcode/leetcode15/Leetcode1429.java
+++ src/leetcode/leetcode15/Leetcode1429.java
@@ -6,32 +6,50 @@ import java.util.*;
 
 public class Leetcode1429 {
 
+    class Node{
+        int val;
+        Node pre;
+        Node next;
+        public Node(int val){
+            this.val=val;
+        }
+    }
+
     class FirstUnique {
 
-        LinkedHashSet<Integer> set= new LinkedHashSet<>();
-        Set<Integer> remove=new HashSet<>();
+        Map<Integer,Node> map=new HashMap<>();
+        Node first;
+        Node last;
         public FirstUnique(int[] nums) {
+            this.first=new Node(-1);
+            this.last=new Node(-1);
+            first.next=last;
+            last.pre=first;
             for(int num:nums)
                 add(num);
         }
 
         public int showFirstUnique() {
-            if(set.isEmpty())
-                return -1;
-            System.out.println(set);
-            Iterator<Integer> i=set.iterator();
-            return i.next();
+            return first.next.val;
         }
 
         public void add(int value) {
-            if(remove.contains(value))
-                return;
-            if(set.contains(value)) {
-                remove.add(value);
-                set.remove(value);
-                return;
+            if(map.containsKey(value)){
+                Node remove=map.get(value);
+                if(remove==null)
+                    return;
+                map.put(value,null);
+                remove.pre.next=remove.next;
+                remove.next.pre=remove.pre;
+            }else{
+                Node add=new Node(value);
+                map.put(value,add);
+                Node l=last.pre;
+                l.next=add;
+                add.pre=l;
+                add.next=last;
+                last.pre=add;
             }
-            set.add(value);
         }
     }
 }
