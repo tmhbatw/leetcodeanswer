d0cf3227e55dbe592397d14641794552673fc0c2
tmhbatw <1060226998@qq.com>
2021-12-14 02:54:56 +0800

    更新了Leetcode 第490题的答案

--- src/leetcode/leetcode5/Leetcode490.java
+++ src/leetcode/leetcode5/Leetcode490.java
@@ -1,8 +1,34 @@
 package leetcode.leetcode5;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Leetcode490 {
 
+    int[][] next=new int[][]{{-1,0},{1,0},{0,1},{0,-1}};
     public boolean hasPath(int[][] maze, int[] start, int[] destination) {
+        int row=maze.length,col=maze[0].length;
+        boolean[][] reached=new boolean[row][col];
+        reached[start[0]][start[1]]=true;
+        Queue<int[]> q=new LinkedList<>();
+        q.add(start);
+
+        while(!q.isEmpty()){
+            int[] cur=q.poll();
+            for(int[] n:next){
+                int i=cur[0],j=cur[1];
+                while(i+n[0]>=0&&i+n[0]<row&&j+n[1]>=0&&j+n[1]<col&&maze[i+n[0]][j+n[1]]==0){
+                    i+=n[0];
+                    j+=n[1];
+                }
+                if(i==destination[0]&&j==destination[1])
+                    return true;
+                if(!reached[i][j]) {
+                    q.add(new int[]{i, j});
+                    reached[i][j]=true;
+                }
+            }
+        }
         return false;
     }
 }
