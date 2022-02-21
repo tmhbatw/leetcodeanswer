32408ed9150d32dff5e6b26b0ed90a7a292cbe40
tmhbatw <1060226998@qq.com>
2022-02-21 19:59:14 +0800

    更新了Leetcode 第2176题的答案

---
+++ src/leetcode/leetcode22/Leetcode2176.java
@@ -0,0 +1,16 @@
+package leetcode.leetcode22;
+
+public class Leetcode2176 {
+
+    public int countPairs(int[] nums, int k) {
+        int result=0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]==nums[j]&&i*j%k==0){
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+}
