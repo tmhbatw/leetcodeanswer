e14f451d9973c51b3711bdfae917b7fb79835701
tmhbatw <1060226998@qq.com>
2022-06-27 19:20:57 +0800

    更新了Leetcode 第2319题的答案

---
+++ src/leetcode/leetcode23/Leetcode2319.java
@@ -0,0 +1,21 @@
+package leetcode.leetcode23;
+
+public class Leetcode2319 {
+
+    public boolean checkXMatrix(int[][] grid) {
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(i==j||i+j==grid.length-1) {
+                    if (grid[i][j] == 0)
+                        return false;
+                }else{
+                    if(grid[i][j]!=0)
+                        return false;
+                }
+
+            }
+        }
+
+        return true;
+    }
+}
