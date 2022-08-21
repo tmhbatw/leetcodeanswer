2cc4fab072aecd55e77193956f556ad695c8179b
liuruihong.1 <liuruihong.1@bytedance.com>
2022-08-21 14:32:46 +0800

    更新了leetcode2370题的答案

---
+++ src/leetcode/leetcode22/Leetcode2192.java
@@ -0,0 +1,25 @@
+package leetcode.leetcode22;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Leetcode2192 {
+
+    public List<List<Integer>> getAncestors(int n, int[][] edges) {
+        List<List<Integer>> list=new ArrayList<>();
+        List<Set<Integer>> result=new ArrayList<>();
+
+        for(int i=0;i<n;i++){
+            list.add(new ArrayList<>());
+            result.add(new HashSet<>());
+        }
+
+        for(int[] edge:edges){
+            list.get()
+        }
+    }
+
+
+}
