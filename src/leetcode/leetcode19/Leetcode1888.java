ccc550d0b7df0638f1fef6ccfd28a53952eff6f4
liuruihong.1 <liuruihong.1@bytedance.com>
2022-10-22 21:30:54 +0800

    更新了leetcode第1888题的答案

--- src/leetcode/leetcode19/Leetcode1888.java
+++ src/leetcode/leetcode19/Leetcode1888.java
@@ -3,21 +3,65 @@ package leetcode.leetcode19;
 public class Leetcode1888 {
 
     public int minFlips(String s) {
-        int min=Math.min(getCount(s,'0'),getCount(s,'1'));
-        s=s.substring(1)+s.charAt(0);
-        int min2=Math.min(getCount(s,'0'),getCount(s,'1'));
-        s=s.substring(1)+s.charAt(0);
-        int min3=Math.min(getCount(s,'0'),getCount(s,'1'));
-        min=Math.min(min,min3);
-        return Math.min(min,min2);
+        int length = s.length();
+        //偶数位置0、1的个数
+        int even0 = 0;
+        int even1 = 0;
+        //奇数位置0、1的个数
+        int odd0 = 0;
+        int odd1 = 0;
+
+        for (int i = 0; i < length; i++) {
+            if (i % 2 == 0) {
+                if (s.charAt(i) == '0')
+                    even0++;
+                else
+                    even1++;
+            } else {
+                if (s.charAt(i) == '0')
+                    odd0++;
+                else
+                    odd1++;
+            }
+        }
+
+        int res = Math.min(even1 + odd0, even0 + odd1);
+        if (length % 2 == 0) {
+            return res;
+        }
+
+        for (int i = 0; i < length; i++) {
+
+            if (i % 2 == 0) {
+                if (s.charAt(i) == '0') {
+                    even0--;
+                    odd0++;
+                } else {
+                    even1--;
+                    odd1++;
+                }
+            } else {
+                if (s.charAt(i) == '1') {
+                    odd1--;
+                    even1++;
+                } else {
+                    odd0--;
+                    even0++;
+                }
+            }
+
+            res = Math.min(res, Math.min(even1 + odd0, even0 + odd1));
+        }
+
+        return res;
     }
 
-    private int getCount(String s, char cur){
-        int count =0;
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)!=cur)
+    private int getCount(String s, char cur) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != cur)
                 count++;
-            cur = cur=='0'?'1':'0';
+            cur = cur == '0' ? '1' : '0';
         }
         return count;
     }
