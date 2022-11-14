30272b18a00a697703022ae6c287dd5b831be176
liuruihong.1 <liuruihong.1@bytedance.com>
2022-11-14 19:48:12 +0800

    更新了leetcode第2469题的答案

---
+++ src/leetcode/leetcode25/Leetcode2469.java
@@ -0,0 +1,10 @@
+package leetcode.leetcode25;
+
+public class Leetcode2469 {
+
+
+    public double[] convertTemperature(double celsius) {
+        return new double[]{celsius + 273.15, celsius * 1.8 + 32};
+    }
+
+}
