52dd021773f0de461655881cfd5d32aaf51e044a
liuruihong.1 <liuruihong.1@bytedance.com>
2023-06-24 22:07:34 +0800

    更新了Leetcode2698题的答案，很久没见

---
+++ src/leetcode/leetcode27/Leetcode2698.java
@@ -0,0 +1,14 @@
+package leetcode.leetcode27;
+
+public class Leetcode2698 {
+
+    public int minLength(String s) {
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.startsWith("AB", i) || s.startsWith("CD", i)) {
+                return minLength(s.substring(0, i) + s.substring(i + 2));
+            }
+        }
+        return s.length();
+    }
+
+}
