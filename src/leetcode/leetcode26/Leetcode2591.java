ac3bf4c0778d6d7f2bded4232966f88d673170e0
liuruihong.1 <liuruihong.1@bytedance.com>
2023-10-02 23:22:33 +0800

    更新了leetcode2591题的答案

--- src/leetcode/leetcode26/Leetcode2591.java
+++ src/leetcode/leetcode26/Leetcode2591.java
@@ -9,10 +9,10 @@ public class Leetcode2591 {
         if (money > children * 8) {
             return children - 1;
         }
-        if (money == children * 7 - 4) {
-            return money / 7 - 1;
+        if (money == children * 8 - 4) {
+            return money / 8 - 1;
         }
+        money -= children;
         return money / 7;
     }
-
 }
